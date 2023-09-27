ef991b5dda4b55578b20331bc464c9639fa97145
Shubham Yadav <shubham.12114557@lpu.in>
2023-09-28 00:21:06 +0530

    Update J9.java

---
+++ J/src/Ayan.java
@@ -0,0 +1,5 @@
+public class Ayan {
+    public static void main(String[] args) {
+        System.out.println("hello shekhu");
+    }
+}
--- J/src/J9.java
+++ J/src/J9.java
@@ -3,7 +3,7 @@ package J.src;
 public class J9 {
     public static void main(String[] args) {
         for (int counter=0;counter<5;counter=counter+1){
-            System.out.print("j");
+            System.out.println("j");
         }
     }
 }
