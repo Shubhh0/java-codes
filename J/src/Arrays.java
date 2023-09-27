85c8e84a43b1384e43206255c49848a784143c03
Shubham Yadav <shubham.12114557@lpu.in>
2023-09-28 00:33:26 +0530

    Arrays

---
+++ J/src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int marks[]= new int[3];
+        marks[0]=65;//maths
+        marks[1]=75;//english
+        marks[2]=95;//hindi
+        System.out.println(marks[1]);
+        System.out.println(marks[0]);
+        System.out.println(marks[2]);
+
+    }
+}
